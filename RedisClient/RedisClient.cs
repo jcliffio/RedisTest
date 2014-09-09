ca9f41a2f985a4ae6363ec55afe75770bd377e06
Josh Clifford <joshua.clifford91@gmail.com>
2014-09-08 21:09:30 -0500

    First actual commit of my RedisTest. Just built a simple client project with an asp.net mvc project on top of it

---
+++ RedisClient/RedisClient.cs
@@ -0,0 +1,38 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisClientProject
+{
+    public class RedisClient
+    {
+        private static ConnectionMultiplexer redis;
+
+        public RedisClient()
+        {
+            if (redis == null)
+            {
+                redis = ConnectionMultiplexer.Connect("localhost");
+            }
+        }
+
+        public void SetStringByKey(string key, string value)
+        {
+            var db = redis.GetDatabase(0);
+
+            db.StringSet(key, value);
+        }
+
+        public string GetStringByKey(string key)
+        {
+            var db = redis.GetDatabase(0);
+
+            var result = db.StringGet(key);
+
+            return result;
+        }
+    }
+}
---
+++ RedisTest/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RedisClientProject;
+using RedisTest.Models.Home;
+
+namespace RedisTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var redisClient = new RedisClient();
+
+            redisClient.SetStringByKey("jcliffio:test-time", DateTime.Now.ToString());
+
+            var cachedTime = redisClient.GetStringByKey("jcliffio:test-time");
+
+            var indexViewModel = new IndexViewModel
+            {
+                CachedTime = cachedTime
+            };
+
+            return View(indexViewModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
